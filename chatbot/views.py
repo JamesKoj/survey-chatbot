26e2fd6d7c1a5d71c163447fbf5c88ee0dc15dbb
JamesKoj <41268720+JamesKoj@users.noreply.github.com>
2021-04-26 12:59:22 -0400

    refactored loading of tensorflow model

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,27 +1,20 @@
 from django.http import JsonResponse
 import numpy as np
-import tensorflow.compat.v1 as tf
-# from tensorflow.compat.v1.keras.backend import set_session
-import json
-import pickle
+import chatbotAPI.settings as chatbotSett
 import chatbot.helpers
 from .questions import switch_question
-# third party imports
+# third party imports 
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import PostSerializer
 from .models import Post
 
-global sess, trained_model, graph
 # Why was session not necessary on heroku, probably because we're using Tensorflow 2.0
 # sess = tf.Session()
 # set_session(sess)
 
 # graph = tf.get_default_graph()
-trained_model = tf.keras.models.load_model('chatbot/chatbotmodel_Q1.h5')
 
-intents = json.loads(open('chatbot/intents2.json').read())
-words = pickle.load(open('chatbot/words.pkl','rb'))
 
 
 # Create your views here.
@@ -31,13 +24,13 @@ class ChatbotView(APIView):
         if serializer.is_valid():
             raw_sentence = serializer.data["user_response"]
             sentence = chatbot.helpers.clean_up_sentence(raw_sentence)
-            sentence_array = chatbot.helpers.bow(sentence, words, show_details=True)
+            sentence_array = chatbot.helpers.bow(sentence, chatbotSett.words, show_details=True)
             # with graph.as_default():
                 # set_session(sess)
-            sentiment_probabilities = trained_model.predict(np.array([sentence_array]))[0]
+            sentiment_probabilities = chatbotSett.trained_model.predict(np.array([sentence_array]))[0]
             # returns a list of predictions with an error threshold of 0.57
             prediction = chatbot.helpers.predict_class(sentiment_probabilities, 0.57)
-            res = chatbot.helpers.getResponse(prediction, intents)
+            res = chatbot.helpers.getResponse(prediction, chatbotSett.intents)
             # full prediction list without an error threshold to add to API endpoint
             chatbot_predictions = chatbot.helpers.predict_class(sentiment_probabilities, 0)
             return JsonResponse(switch_question(serializer.data["qid"], res["tag"], chatbot_predictions))
